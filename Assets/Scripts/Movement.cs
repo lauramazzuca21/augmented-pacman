6f0c02df9031807db4890696cc5a3862f01dc638
Laura Mazzuca <lauramazzuca@protonmail.com>
2022-03-18 18:37:41 +0100

    completes car movement thus closes #1

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -22,25 +22,7 @@ public class Movement : MonoBehaviour
     //FixedUpdate gets called before Update
     void FixedUpdate()
     {
-        // Bit shift the index of the layer (8) to get a bit mask
-        int layerMask = 1 << 8;
 
-        // This would cast rays only against colliders in layer 8.
-        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-        layerMask = ~layerMask;
-
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Debug.Log("Did Hit");
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            Debug.Log("Did not Hit");
-        }
     }
 
     // Update is called once per frame
@@ -78,6 +60,6 @@ public class Movement : MonoBehaviour
         else
             newPos = new Vector3(_joystick.Horizontal * _speed * Time.deltaTime, 0.0f, 0.0f);
 
-        transform.position = new Vector3(transform.position.x, transform.position.y, 10.0f) + newPos;
+        transform.position = transform.position + newPos;
     }
 }
